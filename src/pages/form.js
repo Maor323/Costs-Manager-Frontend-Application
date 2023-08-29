8407e7fae68209634653eb31d586ffaa1afc0547
maorh <maorhylaptop@gmail.com>
2023-08-29 14:55:07 +0300

    indexedDB

--- src/pages/form.js
+++ src/pages/form.js
@@ -2,26 +2,26 @@
 import React from 'react'
 import { useState } from 'react';
 import { nanoid } from "nanoid";
-
-
+import '../components/buttonstyle.css'
+import './form.css'
 
 function Form() {
 
     // all the information we want to be saved each render
     const [Category, setCategory] = useState("Food");
-    const [startDate, setStartDate] = useState();
+    const [startDate, setStartDate] = useState("");
     const [Price, setPrice] = useState("");
     const [Description, setDescription] = useState("");
 
     // add button was pressed
-    function handleClicks() {
+    async function handleClicks() {
         let ID = nanoid();
         const costData = {
             ID: ID,
-            category: Category,
             price: Price,
-            date: startDate,
-            description: Description
+            category: Category,
+            description: Description,
+            date: startDate
         }
         //check valid
         if (!costData.price || !costData.date) {
@@ -29,6 +29,50 @@ function Form() {
             return;
         }
         // insert the data
+        try {
+
+            // Open IndexedDB
+            const db = await openDB("CostDatabase", 1);
+
+            // Create a transaction and object store
+            const tx = db.transaction("costs", "readwrite");
+            const store = tx.objectStore("costs");
+
+            // Add the data
+            await store.add(costData);
+
+            // Complete the transaction
+            await tx.done;
+
+            console.log("Data added to IndexedDB successfully!");
+
+            // Clear the form
+            setCategory("Food");
+            setStartDate("");
+            setPrice("");
+            setDescription("");
+        } catch (error) {
+            console.error("Error adding data to IndexedDB:", error);
+            alert("Error adding data to IndexedDB:", error);
+        }
+        async function openDB(dbName, version) {
+            return new Promise((resolve, reject) => {
+                const request = indexedDB.open(dbName, version);
+
+                request.onupgradeneeded = (event) => {
+                    const db = event.target.result;
+                    db.createObjectStore("costs", { keyPath: "ID" });
+                };
+
+                request.onsuccess = (event) => {
+                    resolve(event.target.result);
+                };
+
+                request.onerror = (event) => {
+                    reject(event.target.error);
+                };
+            });
+        }
 
     }
     return (
@@ -61,7 +105,7 @@ function Form() {
                         <label>Description
                             <input name="Description" id="Description" value={Description} onChange={(e) => setDescription(e.target.value)} /></label>
                     </div>
-                    <button className="addButton" onClick={handleClicks}> Add </button>
+                    <button className="add-btn" onClick={handleClicks}> Add </button>
                 </form>
 
             </div>
